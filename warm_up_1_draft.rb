c359e19b77d6401f709f03196a2ba0ffd0b18e80
David Wiesenberg <david_britta@yahoo.com>
2017-01-26 14:01:03 -0500

    intermediate

--- warm_up_1_draft.rb
+++ warm_up_1_draft.rb
@@ -6,8 +6,9 @@ def parse_tag(node_string)
 
   Node = Struct.new(:tag, :attributes, :parent, :children)
 
-  children = []
-  attributes = {}
+  node_root = Node.new(tag_name, attr_hash, parent, children)
+
+  node_root.children = []
 
   puts "\nnode_string = #{node_string}\n" 
 
@@ -15,20 +16,18 @@ def parse_tag(node_string)
   puts "\nnode_array = #{node_array}\n" 
   node_array.each_with_index do |i, element|   # format correct?
     if i == 0
-      tag = element
+      node_root.tag = element
     else
       attribute_key = element.split("=")[0]
       attribute_value = element.split("=")[1][1..-2] # omit quotes
-      attributes[attribute_key] = attribute_value
+      node_root.attributes[attribute_key] = attribute_value
     end
-    puts "tag = #{tag}"
+    puts "node_root.tag = #{node_root.tag}"
     puts "attribute_key = #{attribute_key} ... attribute_value = #{attribute_value}"
+
   end
-  parent = nil
-  children = [nil]
-
-  node_root = Node.new(tag, attributes, parent, children)
-
+  node_root.parent = nil
+  node_root.children = [nil]
   puts "node_root = #{node_root}"
   node_root
 
